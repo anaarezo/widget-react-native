d95aa11d28ae65203742ad4a82c50067c7c82b6a
anaarezo <laura.arezo@gmail.com>
2025-07-13 12:20:37 +0100

    feat: WIDGET-5 widget basic on iOS

---
+++ ios/Greeting/Model/GreetingEntryModel.swift
@@ -0,0 +1,13 @@
+//
+//  GreetingEntryModel.swift
+//  GreetingExtension
+//
+//  Created by Ana Arezo on 13/07/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct GreetingEntryModel: TimelineEntry {
+    let date: Date
+}
